cba669d8ba9dea0ceafc70ce86635e7d9523bb64
TienDat11 <ngothiendat258@gmail.com>
2024-11-15 13:53:37 +0700

    fix header

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+    <header className="sticky top-0 z-10 w-full bg-card shadow-sm">
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-center gap-5 px-5 py-3">
         <Link href="/" prefetch={true} className="text-2xl font-bold text-primary">
-        Conjob
+          Conjob
         </Link>
         <SearchField />
         <UserButton className="sm:ms-auto" />
